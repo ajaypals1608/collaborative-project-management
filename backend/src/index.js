ed684c78d12de338ff97fd0f2a70e5dd6ab45717
Ajaypal Singh <ajaypals1608@gmail.com>
2026-05-25 10:26:30 +0000

    refactored routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import healthRoutes from "./routes/healthRoutes.js";
 const app = express();
 const PORT = 3000;
 
@@ -8,31 +9,14 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get("/health", (req, res) => {
-	console.log("Health endpoint hit");
-	res.json({
-		status: "ok",
-		message: "server running",
-	});
-});
-
-app.get("/", (req, res) => {
-	res.send("Welcome to collaborative PM backend");
-});
-
-app.get("/users/:id", (req, res) => {
-	res.json({
-		userId: req.params.id,
-	});
-});
-
-app.post("/test", (req, res) => {
+/* app.post("/test", (req, res) => {
 	console.log(req.body);
 	res.json({
 		receivedData: req.body,
 	});
-});
+}); */
 
+app.use("/health", healthRoutes);
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
