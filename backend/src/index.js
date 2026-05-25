de09176092e968c31c6353a846126b01ea31bcf0
Ajaypal Singh <ajaypals1608@gmail.com>
2026-05-25 08:09:07 +0000

    added middleware basics

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,12 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+});
+
 app.get("/health", (req, res) => {
 	console.log("Health endpoint hit");
 	res.json({
@@ -19,6 +25,14 @@ app.get("/users/:id", (req, res) => {
 		userId: req.params.id,
 	});
 });
+
+app.post("/test", (req, res) => {
+	console.log(req.body);
+	res.json({
+		receivedData: req.body,
+	});
+});
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
